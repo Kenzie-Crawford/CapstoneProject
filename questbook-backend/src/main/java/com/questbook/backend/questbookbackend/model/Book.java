1347975e2be23c120b1680f694e49e8a63eddcf8
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-03-23 15:59:02 -0400

    Review model class

--- questbook-backend/src/main/java/com/questbook/backend/questbookbackend/model/Book.java
+++ questbook-backend/src/main/java/com/questbook/backend/questbookbackend/model/Book.java
@@ -1,5 +1,7 @@
 package com.questbook.backend.questbookbackend.model;
+
 import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -10,7 +12,7 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column (name = "open_library_id", unique = true)
+    @Column(name = "open_library_id", unique = true)
     private String openLibraryId;
 
     @Column(nullable = false)
@@ -19,20 +21,101 @@ public class Book {
     @Column(nullable = false)
     private String author;
 
-    @Column (nullable = false)
+    @Column(nullable = false)
     private String genre;
 
-    @Column (columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT")
     private String description;
 
-    @Column (name = "published_date")
+    @Column(name = "published_date")
     private LocalDate publishedDate;
 
+    @Column
     private String source;
 
-    @Column (name = "created_at")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @Column (name = "updated_at")
-    private LocalDateTime upatedAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOpenLibraryId() {
+        return openLibraryId;
+    }
+
+    public void setOpenLibraryId(String openLibraryId) {
+        this.openLibraryId = openLibraryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
---
+++ questbook-backend/src/main/java/com/questbook/backend/questbookbackend/model/Review.java
@@ -0,0 +1,80 @@
+package com.questbook.backend.questbookbackend.model;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column (columnDefinition = "TEXT")
+    private String comment;
+
+    @Column (name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column (name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
