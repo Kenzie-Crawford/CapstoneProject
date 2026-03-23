d62267c21d0686a91609beadddf9b5e248e331ee
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-03-23 16:14:11 -0400

    userBooks model class

---
+++ questbook-backend/src/main/java/com/questbook/backend/questbookbackend/model/UserBook.java
@@ -0,0 +1,114 @@
+package com.questbook.backend.questbookbackend.model;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_books", uniqueConstraints =
+@UniqueConstraint(columnNames = {"user_id", "book_id"}))
+
+
+public class UserBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column (nullable = false)
+    private String status;
+
+    @Column (name = "user_rating")
+    private Integer userRating;
+
+    @Column (name = "started_at")
+    private LocalDate startedAt;
+
+    @Column (name = "finished_at")
+    private LocalDate finishedAt;
+
+    @Column (name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column (name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(int userRating) {
+        this.userRating = userRating;
+    }
+
+    public LocalDateTime getStartedAt() {
+        return startedAt;
+    }
+
+    public void setStartedAt(LocalDateTime startedAt) {
+        this.startedAt = startedAt;
+    }
+
+    public LocalDateTime getFinishedAt() {
+        return finishedAt;
+    }
+
+    public void setFinishedAt(LocalDateTime finishedAt) {
+        this.finishedAt = finishedAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
